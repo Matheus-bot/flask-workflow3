730d3c39da4f7d2ee298606de7d804f9329ba2c5
Matheus <matheushenrry00@gmail.com>
2026-05-12 11:42:29 -0300

    chore: prepara aplicação para deploy

--- app.py
+++ app.py
@@ -26,6 +26,8 @@ def contato():
 def cadastro_livro():
     return "Página de cadastro de livros"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+import os
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
