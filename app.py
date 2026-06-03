356a3def0ace585bab286122663223acd4704716
Matheus Henrique <62816022+Matheus-bot@users.noreply.github.com>
2026-06-03 19:28:59 -0300

    Update Flask app to use environment port

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -27,4 +28,5 @@ def cadastro_livro():
     return "Página de cadastro de livros"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
